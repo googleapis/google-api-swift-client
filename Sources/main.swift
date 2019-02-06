430916a186674931f187094bb33fb9595dc66c5d
Tim Burks <timburks@google.com>
2019-02-06 11:06:15 -0800

    Add license headers, build generator in debug mode by default.

--- Sources/main.swift
+++ Sources/main.swift
@@ -14,6 +14,26 @@
 
 import Foundation
 
+// This license is prepended to generated files.
+// You are welcome to replace it if you are generating clients for non-Google APIs.
+let License = """
+// Copyright 2019 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// NOTE: This file is automatically-generated!
+"""
+
 extension String {
   func capitalized() -> String {
     return prefix(1).capitalized + dropFirst()
@@ -256,7 +276,8 @@ class Service : Codable {
     guard let schemas = schemas else {
       return ""
     }
-    var s = ""
+    var s = License
+    s += "\n"
     s += "import Foundation\n"
     s += "import OAuth2\n"
 

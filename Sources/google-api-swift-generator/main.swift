ebc473363f17a8580841da7256e7f142d2f922d7
Mark McCracken <markmccracken91@me.com>
2020-01-23 18:27:41 +0000

    Added better encoding for Double and alias for top level schemas of t… (#10)
    * Added documentation for getting started with contributing
    
    * Added public initializers for structs
    
    * Added public initializers for structs, for array items
    
    * Added better encoding for Double and alias for top level schemas of type alias
    
    Co-authored-by: Tim Burks <timburks@google.com>

--- Sources/google-api-swift-generator/main.swift
+++ Sources/google-api-swift-generator/main.swift
@@ -15,6 +15,11 @@
 import Foundation
 import Discovery
 
+enum ParsingError: Error {
+  case topLevelSchemaUnknownType(schemaName: String, type: String)
+  case topLevelSchemaArrayDoesNotContainObjects(schemaName: String, type: String)
+}
+
 func createStructInitLines(baseIndent: Int, parameters: [String: Schema]) -> String {
   var currentIndent = baseIndent
   var initDeclaration = String(repeating: " ", count: currentIndent) + "public init ("
@@ -113,7 +118,7 @@ extension Discovery.Resource {
 }
 
 extension Discovery.Service {
-  func generate() -> String {
+  func generate() throws -> String {
     guard let schemas = schemas else {
       return ""
     }
@@ -164,12 +169,15 @@ extension Discovery.Service {
               }
               s.addLine("}")
             default:
-              s.addLine("ERROR-UNHANDLED-ARRAY-TYPE \(itemType)")
+              throw ParsingError.topLevelSchemaArrayDoesNotContainObjects(schemaName: schema.key, type: itemType)
             }
           }
         }
+      case "any":
+        s.addLine()
+        s.addLine(indent: 2, "public typealias `\(schema.key)` = JSONAny")
       default:
-        s.addLine("ERROR-UNHANDLED-SCHEMA-VALUE-TYPE \(schema.key) \(String(describing:schema.value.type))")
+        throw ParsingError.topLevelSchemaUnknownType(schemaName: schema.key, type: schema.value.type ?? "nil - unknown type")
       }
     }
     
@@ -193,7 +201,7 @@ func main() throws {
   let decoder = JSONDecoder()
   do {
     let service = try decoder.decode(Service.self, from: data)
-    let code = service.generate()
+    let code = try service.generate()
     print(code)
   } catch {
     print("error \(error)\n")

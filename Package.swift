fb56f074fa4e47032287748531ab13ca9a52b7c9
Tim Burks <timburks@google.com>
2020-03-30 16:34:50 -0700

    Update Package.swift to complete pr#16

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     .library(name: "Discovery", targets: ["Discovery"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/googleapis/google-auth-library-swift.git", from: "0.4.2"),
+    .package(url: "https://github.com/googleapis/google-auth-library-swift.git", from: "0.5.2"),
   ],
   targets: [
     .target(name: "google-api-swift-generator", dependencies: ["Discovery"], path: "Sources/google-api-swift-generator"),
